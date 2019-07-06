04bff43859f42711d176d118856672eab1b81064
sunsun4343 <sunsun4343@gmail.com>
2019-07-07 00:26:56 +0900

    리소스

---
+++ I'm Load/Assets/Scripts/UI/ResourceInfo/ResourceInfoItemUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceInfoItemUI : MonoBehaviour
+{
+    [SerializeField] Image Image_Icon;
+    [SerializeField] Text Text_Count;
+
+
+
+}
